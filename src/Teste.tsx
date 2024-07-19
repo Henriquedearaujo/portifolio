1a13c0eddd4fe7e5755d1f36b0917503a68580f6
Henriquedearaujo <ricke_cris@hotmail.com>
2024-07-19 15:03:27 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/Teste.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Teste() {
+  return <Botao>Clique aqui</Botao>
+}
+
+export default Teste
---
+++ src/styles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: o;
+  }
+`
+export default EstiloGlobal
